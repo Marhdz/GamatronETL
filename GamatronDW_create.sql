1629cf3a0b9eec378914d6cfda239efae3ddcb7d
Michelle Romero <marhdz0@gmail.com>
2024-10-25 19:26:57 -0600

    proceso incremental

--- GamatronDW_create.sql
+++ GamatronDW_create.sql
@@ -1,41 +1,61 @@
 --/* clean tables */
---drop TABLE IF EXISTS DimTime;
---drop TABLE IF EXISTS DimCarrier;
---drop TABLE IF EXISTS FactFlight;
+drop TABLE IF EXISTS DimTiempo;
+drop TABLE IF EXISTS DimAerolinea;
+drop TABLE IF EXISTS FactVuelo;
 
-CREATE TABLE DimTiempo(
-	tiempoKey int PRIMARY KEY,
-	fechaCompleta varchar(10),
-	anio int,
-	trimestre int,
-	mes int,
-	nombreMes varchar(10),
-	diaDeMes int,
-	diaDeSemana int,
-	nombreDia varchar(10) --Lunes 1
-);
+/* DDL for the date dimension */
+create table DimTiempo (
+date_key int not null,
+full_date smalldatetime,
+day_of_week tinyint,
+day_num_in_month tinyint,
+day_num_overall smallint,
+day_name varchar(9),
+day_abbrev char(3),
+weekday_flag varchar(8),
+week_num_in_year tinyint,
+week_num_overall smallint,
+week_begin_date smalldatetime,
+week_begin_date_key int,
+month tinyint,
+month_num_overall smallint,
+month_name varchar(9),
+month_abbrev char(3),
+quarter tinyint,
+year smallint,
+yearmo int,
+fiscal_month tinyint,
+fiscal_quarter tinyint,
+fiscal_year smallint,
+last_day_in_month_flag varchar(15),
+same_day_year_ago_date smalldatetime,
+primary key (date_key))
+;
 
 CREATE TABLE DimAerolinea(
-	aerolineaKey int PRIMARY KEY,
-	codigoAerolinea int,
-	DOTIDAerolinea int,
-	nombreAerolinea varchar(50)
+	aerolineaKey int identity(1,1) PRIMARY KEY,
+	codigoAerolinea varchar(50),
+	nombreAerolinea varchar(100)
 );
 
 CREATE TABLE FactVuelo(
 	tiempoKey int,
 	aerolineaKey int,
-	tailNumber int,
-	AeropuertoOrigenID int,
-	CiudadOrigen varchar(3),
+	aerolineaNombre varchar(100),
+	tailNumber varchar(10),
+
+	aeropuertoOrigenID int,
+	ciudadOrigen varchar(10),
 	nombreCiudadOrigen varchar(50),
-	EstadoOrigen varchar(3),
+	estadoOrigen varchar(10),
 	nombreEstadoOrigen varchar(50),
+
 	aeropuertoDestinoID int,
-	ciudadDestino varchar(3),
+	ciudadDestino varchar(10),
 	nombreCiudadDestino varchar(50),
-	estadoDestino varchar(3),
+	estadoDestino varchar(10),
 	nombreEstadoDestino varchar(50),
+
 	CRSHoraSalida varchar(5),
 	horaSalidaActual varchar(5),
 	difHoraSalida int,
@@ -43,15 +63,19 @@ CREATE TABLE FactVuelo(
 	atrasoSalidaMayorDe15min varchar(10),
 	atrasoSalidaEnIntervalo varchar(50),
 	CRSHoraSalidaEnIntervalo varchar(50),
+
 	CRSHoraLlegada varchar(5),
 	horaLlegadaActual varchar(5),
+	diffHoraLlegada int,
 	atrasoLlegadaEnMin int,
-	atrasoLlegadaMayor15Min int,
+	atrasoLlegadaMayor15Min varchar(10),
 	atrasoLlegadaEnIntervalo varchar(50),
 	CRSHoraLlegadaEnIntervalo varchar(50),
-	cancelado varchar(10),
-	codigoCancelado varchar(3),
+
+	cancelado varchar(20),
+	codigoCancelado varchar(10),
 	diferido varchar(10),
+
 	CRSDuracion varchar(10),
 	duracionActual varchar(10), --si cancelado 0, si diferido que llegó a su destino viene de DivActualElapseTime
 	tiempoEnAire int,
@@ -63,7 +87,7 @@ CREATE TABLE FactVuelo(
 	atrasoSeguridad int,
 	atrasoAeronave int,
 	cantAterrizajesDiferidosEnAeropuerto int,
-	vueloDiferidoLlegoADestino varchar(5),
+	vueloDiferidoLlegoADestino varchar(10),
 	duracionVueloDiferido int,
 	distanciaAeropuertoDestino int
 );
